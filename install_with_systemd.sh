8042944a0b242f14db988e488ef63953f7cce654
Mohamed Amine Mzoughi <mmzoughi@jeanlutzsa.com>
2021-01-06 15:10:55 +0100

    Added systemd support.

--- install_with_systemd.sh
+++ install_with_systemd.sh
@@ -3,26 +3,20 @@
 BINARY=wfs
 CONFIG=wfs.conf
 TARGETS=targets.txt
-INITSCRIPT=init.d/wfs
+SYSTEMD_INITSCRIPT=systemd/system/wfs
 
 BINARY_TARGET=/usr/bin
-INITSCRIPT_TARGET=/etc/init.d
+SYSTEMD_INITSCRIPT_TARGET=/etc/systemd/system
 CONFIG_TARGET=/etc/wfs
 
 if [ "$1" = "remove" ]
 then
-    if [ -e /etc/debian_version ]
-    then
-        update-rc.d -f wfs remove
-    elif [ -e /etc/redhat-release ]
-    then
-        chkconfig --del wfs
-    fi
+    systemctl disable wfs.service
 
     rm "$BINARY_TARGET/$BINARY"
     rm "$CONFIG_TARGET/$CONFIG"
     rm "$CONFIG_TARGET/$TARGETS"
-    rm $INITSCRIPT_TARGET/`basename $INITSCRIPT`
+    rm $SYSTEMD_INITSCRIPT_TARGET/`basename $SYSTEMD_INITSCRIPT`
     exit
 fi
 
@@ -32,10 +26,10 @@ then
 fi
 
 cp "$BINARY" "$BINARY_TARGET"
-cp "$INITSCRIPT" "$INITSCRIPT_TARGET"
+cp "$SYSTEMD_INITSCRIPT" "$SYSTEMD_INITSCRIPT_TARGET"
 
 chmod 755 "$BINARY_TARGET/$BINARY"
-chmod 755 "$INITSCRIPT_TARGET/`basename $INITSCRIPT`"
+chmod 755 "$SYSTEMD_INITSCRIPT_TARGET/`basename $SYSTEMD_INITSCRIPT`"
 
 if [ -e $CONFIG_TARGET/$CONFIG ]
 then
@@ -53,12 +47,4 @@ else
     cp "$TARGETS" "$CONFIG_TARGET/$TARGETS" 
 fi
 
-if [ -e /etc/debian_version ]
-then
-    update-rc.d wfs defaults 99 10
-elif [ -e /etc/redhat-release ]
-then
-    chkconfig --levels 2345 wfs on
-fi
-
-
+systemctl enable wfs.service
